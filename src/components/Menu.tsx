55a3e7bb8215dde127036720c5114cd934d4b69d
SAEMMM <saem030@naver.com>
2023-08-05 21:52:56 +0900

    [Design] 메뉴 퍼블리싱

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import "../styles/_Menu.scss";
+
+import unfoldActive from "../static/gnb_unfold_active.png";
 
 function Menu() {
   return (
-    <div>Menu</div>
-  )
+    <>
+      <main id="menu">
+        {/* 첫번째 대메뉴 */}
+        <article>
+          <div className="largeMenu">
+            <p>대메뉴</p>
+            <img src={unfoldActive} alt="unfoldActive" />
+          </div>
+          <div className="smallMenu">
+            <p>소메뉴</p>
+          </div>
+          <div className="smallMenu">
+            <p>소메뉴</p>
+          </div>
+        </article>
+
+        <section />
+        
+        {/* 두번째 대메뉴 */}
+        <article>
+          <div className="largeMenu">
+            <p>대메뉴</p>
+            <img src={unfoldActive} alt="unfoldActive" />
+          </div>
+          <div className="smallMenu">
+            <p>소메뉴</p>
+          </div>
+          <div className="smallMenu">
+            <p>소메뉴</p>
+          </div>
+        </article>
+      </main>
+    </>
+  );
 }
 
-export default Menu
+export default Menu;
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import "../styles/_Main.scss";
+import Menu from "../components/Menu";
+import Header from "../components/Header";
+import Content from "../components/Content";
 
 function Main() {
   return (
-    <div>Main</div>
-  )
+    <div className="container">
+      <Menu />
+      <Header />
+      <Content />
+    </div>
+  );
 }
 
-export default Main
+export default Main;
