5680afe0744129c3524783cf8c3b93b811f56e34
SAEMMM <saem030@naver.com>
2023-08-07 10:31:27 +0900

    [Design] react-datepicker

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,54 +1,67 @@
 import React from "react";
 import "../styles/_Content.scss";
+import Datepicker from "./Datepicker";
 
 function Content() {
   return (
     <>
       <main id="content">
         <article>
-          <div>
+          <div className="infoBox">
             <p id="infoName">정보1</p>
             <span>정보 내용</span>
           </div>
 
-          <div>
+          <div className="infoBox">
             <p>정보2</p>
             <input type="text" />
           </div>
 
-          <div>
+          <div className="infoBox">
             <p>정보3</p>
             <span>정보 내용</span>
           </div>
 
-          <div>
+          <div className="infoBox">
             <p>정보4</p>
             <input type="text" />
           </div>
 
-          <div>
+          <div className="infoBox">
             <p>날짜</p>
-            <input type="date" />
+            <Datepicker />
           </div>
 
-          <div>
+          <div className="infoBox">
             <p>정보5</p>
-            <input type="radio" />
-            <label>선택1</label>
-            <input type="radio" />
-            <label>선택2</label>
-            <input type="radio" />
-            <label>선택3</label>
+            <div className="radioNcheckBox">
+              <input type="radio" />
+              <label>선택1</label>
+            </div>
+            <div className="radioNcheckBox">
+              <input type="radio" />
+              <label>선택2</label>
+            </div>
+            <div className="radioNcheckBox">
+              <input type="radio" />
+              <label>선택3</label>
+            </div>
           </div>
 
-          <div>
+          <div className="infoBox">
             <p>정보6</p>
-            <input type="checkbox" />
-            <label>선택1</label>
-            <input type="checkbox" />
-            <label>선택2</label>
-            <input type="checkbox" />
-            <label>선택3</label>
+            <div className="radioNcheckBox">
+              <input type="checkbox" />
+              <label>선택1</label>
+            </div>
+            <div className="radioNcheckBox">
+              <input type="checkbox" />
+              <label>선택2</label>
+            </div>
+            <div className="radioNcheckBox">
+              <input type="checkbox" />
+              <label>선택3</label>
+            </div>
           </div>
         </article>
       </main>
---
+++ src/components/Datepicker.tsx
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import 'react-datepicker/dist/react-datepicker.css';
+import { getMonth, getYear } from "date-fns";
+import { range } from "lodash";
+
+function Datepicker() {
+  const [startDate, setStartDate] = useState<Date | null>(new Date());
+
+  const years = range(1990, getYear(new Date()) + 1, 1);
+
+  const months = [
+    "1월",
+    "2월",
+    "3월",
+    "4월",
+    "5월",
+    "6월",
+    "7월",
+    "8월",
+    "9월",
+    "10월",
+    "11월",
+    "12월",
+  ];
+  return (
+    <>
+      <DatePicker
+        renderCustomHeader={({
+          date,
+          changeYear,
+          changeMonth,
+          decreaseMonth,
+          increaseMonth,
+          prevMonthButtonDisabled,
+          nextMonthButtonDisabled,
+        }) => (
+          <div
+            style={{
+              margin: 10,
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <button onClick={decreaseMonth} disabled={prevMonthButtonDisabled}>
+              {"<"}
+            </button>
+            <select
+              value={getYear(date)}
+              onChange={({ target: { value } }) => changeYear(parseInt(value))}
+            >
+              {years.map((option) => (
+                <option key={option} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+
+            <select
+              value={months[getMonth(date)]}
+              onChange={({ target: { value } }) =>
+                changeMonth(months.indexOf(value))
+              }
+            >
+              {months.map((option) => (
+                <option key={option} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+
+            <button onClick={increaseMonth} disabled={nextMonthButtonDisabled}>
+              {">"}
+            </button>
+          </div>
+        )}
+        selected={startDate}
+        onChange={(date) => setStartDate(date)}
+        dateFormat="yyyy.MM.dd"
+      />
+    </>
+  );
+}
+
+export default Datepicker;
