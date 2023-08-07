b0916bd9f03a46a07fdeb648efa321d075b4f05f
SAEMMM <saem030@naver.com>
2023-08-07 20:32:51 +0900

    [Design] 메뉴 fold 디자인

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,24 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/_Menu.scss";
 
 import unfoldActive from "../static/gnb_unfold_active.png";
+import foldActive from "../static/gnb_fold_active.png";
 
 function Menu() {
+  // 대메뉴 열림여부 (fold = true)
+  const [foldLargeMenu, setFoldLargeMenu] = useState(true);
+
+  // 대메뉴 열기
+  const openLargeMenuHandler = () => {
+    setFoldLargeMenu(!foldLargeMenu);
+  };
+
+  console.log("열림여부:", foldLargeMenu);
   return (
     <>
       <main id="menu">
         {/* 첫번째 대메뉴 */}
         <section id="menuGroup">
-          <div className="largeMenu">
+          <div
+            className={
+              foldLargeMenu ? "largeMenu" : "foldLargeMenu + largeMenu"
+            }
+            onClick={openLargeMenuHandler}
+          >
             <p>대메뉴</p>
-            <img src={unfoldActive} alt="unfoldActive" />
+            {foldLargeMenu ? (
+              <img src={unfoldActive} alt="unfoldActive" />
+            ) : (
+              <img src={foldActive} alt="foldActive" />
+            )}
           </div>
-          <div className="smallMenu">
-            <p>소메뉴</p>
-          </div>
-          <div className="smallMenu">
-            <p>소메뉴</p>
-          </div>
+          {foldLargeMenu ? (
+            <>
+              <div className="smallMenu">
+                <p>소메뉴</p>
+              </div>
+              <div className="smallMenu">
+                <p>소메뉴</p>
+              </div>
+            </>
+          ) : (
+            ""
+          )}
         </section>
 
         <section id="paragraphDivision" />
