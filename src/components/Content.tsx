87d4e831eaddb8ec29690f101f4e72fb3eeaba86
SAEMMM <saem030@naver.com>
2023-08-07 23:42:58 +0900

    [Feat] radio 버튼 하나만 선택, 선택3 -> 메세지 출력

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/_Content.scss";
 import Datepicker from "./Datepicker";
 
 function Content() {
+  // radio 버튼 상태 관리
+  const [radioSelected, setRadioSelected] = useState("");
+
+  const radioSelectedHandler = (value: string) => {
+    setRadioSelected(value);
+  };
   return (
     <>
       <main id="content">
@@ -34,20 +40,47 @@ function Content() {
 
           <div className="infoBoxRadio">
             <p>정보5</p>
-            <div className="radioBox">
-              <input type="radio" id="radio1" />
-              <div className="radioDefault" />
-              <label htmlFor="radio1">선택1</label>
-            </div>
-            <div className="radioBox">
-              <input type="radio" id="radio2" />
-              <div className="radioDefault" />
-              <label htmlFor="radio2">선택2</label>
-            </div>
-            <div className="radioBox">
-              <input type="radio" id="radio3" />
-              <div className="radioDefault" />
-              <label htmlFor="radio3">선택3</label>
+            <div id="radioNmsg">
+              <div id="radioSelectGroup">
+                <div className="radioBox">
+                  <input
+                    type="radio"
+                    id="radio1"
+                    checked={radioSelected === "radio1"}
+                    onChange={() => radioSelectedHandler("radio1")}
+                  />
+                  <div className="radioDefault" />
+                  <label htmlFor="radio1">선택1</label>
+                </div>
+                <div className="radioBox">
+                  <input
+                    type="radio"
+                    id="radio2"
+                    checked={radioSelected === "radio2"}
+                    onChange={() => radioSelectedHandler("radio2")}
+                  />
+                  <div className="radioDefault" />
+                  <label htmlFor="radio2">선택2</label>
+                </div>
+                <div className="radioBox">
+                  <input
+                    type="radio"
+                    id="radio3"
+                    checked={radioSelected === "radio3"}
+                    onChange={() => radioSelectedHandler("radio3")}
+                  />
+                  <div className="radioDefault" />
+                  <label htmlFor="radio3">선택3</label>
+                </div>
+              </div>
+
+              {radioSelected === "radio3" ? (
+                <div id="radio3SelectMsg">
+                  <span>* 선택시 텍스트가 표시됩니다.</span>
+                </div>
+              ) : (
+                ""
+              )}
             </div>
           </div>
 
