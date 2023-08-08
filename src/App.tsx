49a71832dd216e2d6da7a286441deb2fdc290394
SAEMMM <saem030@naver.com>
2023-08-08 09:58:15 +0900

    [Feat] react-quert, dev-tool 설치

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Main from "./pages/Main";
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Main />} />
       <Route path="/:id" element={<Main />} />
     </Routes>
   );
